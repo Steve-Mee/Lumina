7d45ebd3aad28b2a5414b27d0e7a26d6c3eb613c
Steve Meerschaut <steve.meerschaut@telenet.be>
2026-04-05 10:46:05 +0200

    build: use pyproject-only metadata via minimal setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,3 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
-setup(
-	name="lumina-bible",
-	version="0.1.0",
-	packages=find_packages(),
-	install_requires=["chromadb", "sentence-transformers"],
-	author="Steve Meerschaut",
-	license="Proprietary",
-	description="Bible & Reflection Engine voor de #1 trading bot ter wereld",
-	long_description=open("README.md", encoding="utf-8").read(),
-	long_description_content_type="text/markdown",
-	url="https://github.com/stevemeerschaut/lumina-bible",
-)
+setup()
